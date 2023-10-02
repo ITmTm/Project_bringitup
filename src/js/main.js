8b823de925f2f0e45eadeb29250b33fbe3a580b7
Timur <Stafr95@mail.ru>
2023-10-02 21:21:28 +0500

    finished playVideo

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 import Slider from "./modules/slider";
+import VideoPlayer from "./modules/playVideo";
 
 document.addEventListener('DOMContentLoaded', function () {
 	'use strict';
 
 	const slider = new Slider('.page', '.next');
 	slider.render();
+
+	const player = new VideoPlayer('.showup .play', '.overlay');
+	player.init();
 
 });
