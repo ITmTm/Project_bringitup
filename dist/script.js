6d9f8fff5a864305d598a7f676d68aa3fa06933e
Timur <Stafr95@mail.ru>
2023-10-03 13:17:09 +0500

    class inheritance

--- dist/script.js
+++ dist/script.js
@@ -68,7 +68,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": function() { return /* binding */ Slider; }
 /* harmony export */ });
 class Slider {
-  constructor(page, btns) {
+  constructor(_ref) {
+    let {} = _ref;
     this.page = document.querySelector(page);
     this.slides = this.page.children;
     this.btns = document.querySelectorAll(btns);
